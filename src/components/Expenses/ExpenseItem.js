aa9b4a53754e5ef0db67d1640ed920fffa5368f3
Sebastián Meza Estrada <sebastian.meza@globant.com>
2023-02-07 16:24:46 -0300

    add: moving components into specific folders

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,8 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
-function ExpenseItem({ title, amount, date }) {
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -12,6 +12,6 @@ function ExpenseItem({ title, amount, date }) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
