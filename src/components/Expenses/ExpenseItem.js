be765920daa3fbf6ad87c25b77b55a096362bb96
Sebastián Meza Estrada <sebastian.meza@globant.com>
2023-02-23 09:25:00 -0300

    add: first useState testing

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [tittle, setTittle] = useState(title);
+
+  const clickHandler = () => {
+    setTittle("New Title");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{tittle}</h2>
         <div className="expense-item__price">$ {amount}</div>
+
+        <button onClick={clickHandler}>Press Me</button>
       </div>
     </Card>
   );
